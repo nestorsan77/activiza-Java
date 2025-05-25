925181eca688c4cb6bb96821ac835d5c2891d9fd
nestorsan77 <nestorsan77@gmail.com>
2025-05-25 18:22:54 +0100

    Primer commit "Subido el proyecto iniciado en el panel de springboot"

---
+++ Activiza/src/main/java/com/activiza/backend/Activiza/ActivizaApplication.java
@@ -0,0 +1,13 @@
+package com.activiza.backend.Activiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivizaApplication.class, args);
+	}
+
+}
